fee857f916548051f90404d9ad2720b9b06ca1a7
Simeon Widdis <sawiddis@gmail.com>
2025-01-18 11:20:03 -0800

    Clean up formatting

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ pub mod model;
 use std::fs::{File, OpenOptions};
 use std::io::{self, BufRead, Read};
 use std::path::{Path, PathBuf};
+use std::time::Instant;
 
 use color_eyre::eyre::{Context, Result};
 use model::Entry;
@@ -43,8 +44,12 @@ fn should_cache(dict_path: &Path, cache_path: &Path) -> Result<bool> {
     }
 
     let (dict_meta, cache_meta) = (
-        dict_path.metadata().wrap_err("failed to read dictionary file metadata")?,
-        cache_path.metadata().wrap_err("failed to read cache file metadata")?,
+        dict_path
+            .metadata()
+            .wrap_err("failed to read dictionary file metadata")?,
+        cache_path
+            .metadata()
+            .wrap_err("failed to read cache file metadata")?,
     );
 
     let dict_mod = match dict_meta.modified() {
@@ -59,12 +64,16 @@ fn should_cache(dict_path: &Path, cache_path: &Path) -> Result<bool> {
         Ok(meta) => meta,
         Err(_) => return Ok(false),
     };
-    
+
     Ok(cache_mod > dict_mod)
 }
 
 fn save_to_cache(trie: &Trie<u8, model::Entry>, cache_path: &Path) -> Result<()> {
-    let cache = OpenOptions::new().create(true).truncate(true).write(true).open(cache_path)?;
+    let cache = OpenOptions::new()
+        .create(true)
+        .truncate(true)
+        .write(true)
+        .open(cache_path)?;
     let _ = postcard::to_io(trie, cache)?;
     Ok(())
 }
@@ -86,20 +95,40 @@ fn load_dictionary() -> Result<Trie<u8, model::Entry>> {
         true => load_from_cache(&cache_path),
         false => {
             let trie = build_trie_from(&path).wrap_err("failed to build dictionary word index")?;
-            save_to_cache(&trie, &cache_path).wrap_err("failed to save the dictionary word index")?;
+            save_to_cache(&trie, &cache_path)
+                .wrap_err("failed to save the dictionary word index")?;
             Ok(trie)
         }
     }
 }
 
 fn main() -> Result<()> {
-    let query = "testament";
     let dict = load_dictionary().wrap_err("failed to load a dictionary")?;
+    let mut res_total = 0;
 
-    let res: Vec<(String, &Entry)> = dict.predictive_search(query).collect();
-    for (_, entry) in res {
-        println!("{entry:?}");
+    let words = [
+        "hurried",
+        "bounce",
+        "goofy",
+        "accidental",
+        "wealth",
+        "point",
+        "seal",
+        "size",
+        "heavenly",
+        "grumpy",
+    ];
+
+    let start = Instant::now();
+    for word in words.iter().cycle().take(10000) {
+        let res: Vec<(String, &Entry)> = dict.predictive_search(word).collect();
+        for (_, entry) in res {
+            res_total += entry.senses.len();
+        }
     }
+    let duration = start.elapsed();
+
+    println!("{:?}\n{}", duration / 10000, res_total);
 
     Ok(())
 }
